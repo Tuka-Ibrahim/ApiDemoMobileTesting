e73ef5cee98505975b46d21eacd93ed5a69e885f
Tuka-Ibrahim <tokaibarahim20@gmail.com>
2022-12-29 17:58:06 +0200

    Second Commit

--- GUI_Web/src/test/java/TestPackage/TestClass.java
+++ GUI_Web/src/test/java/TestPackage/TestClass.java
@@ -16,14 +16,11 @@ import java.net.URL;
 public class TestClass {
     WebDriver driver;
     Home home;
-    Accessibility accessibility;
     AccessibilityNodeProvider accessibilityNodeProvider;
     CustomTittle customTittle;
     CheckBoxPage checkBoxPage;
-    Graphics graphics;
     GraphicsScaleToFit graphicsScaleToFit;
     Views views;
-
 
     @BeforeMethod
     public void setupDevice() throws MalformedURLException {
@@ -36,11 +33,11 @@ public class TestClass {
         caps.setCapability("appium:automationName", "UiAutomator2");
         driver = new AndroidDriver(new URL("http://localhost:4723/"), caps);
         home=new Home(driver);
-        accessibilityNodeProvider=new AccessibilityNodeProvider(driver);
-        customTittle=new CustomTittle(driver);
         checkBoxPage=new CheckBoxPage(driver);
-        graphics=new Graphics(driver);
+        accessibilityNodeProvider=new AccessibilityNodeProvider(driver);
+        checkBoxPage=new CheckBoxPage(driver);
         graphicsScaleToFit=new GraphicsScaleToFit(driver);
+        customTittle=new CustomTittle(driver);
         views=new Views(driver);
         new MobileActions(driver);
     }
